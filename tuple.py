f6b49795b50c1d84a05826a5ec20592bb2bd4c59
Liu Rui <lovearuis@gmail.com>
2018-07-05 11:10:53 +0800

    plusequal.py

---
+++ tuple.py
@@ -0,0 +1,9 @@
+lax_coordinates = (33.9425, -118.408056)
+latitude, longitude = lax_coordinates
+print(latitude)
+print(longitude)
+
+a, b, *rest = range(5)
+print(a, b, rest)
+
+print('{:15}|{:^9}|{:^9}'.format('', 'lat.', 'long.'))

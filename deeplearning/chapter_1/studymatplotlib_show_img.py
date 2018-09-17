c6fedafa9437d89c2d19bcbfdf40daccb2fc148b
Liu Rui <lovearuis@gmail.com>
2018-09-17 14:39:37 +0800

    study deeplearning

---
+++ deeplearning/chapter_1/studymatplotlib_show_img.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+
+img = imread("/Users/liurui/Documents/picture/收藏/一首小诗.png")
+plt.imshow(img)
+
+plt.show()
---
+++ deeplearning/chapter_1/studynumpy.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+# x = np.array([1.0, 2.0, 3.0])
+# y = np.array([2.0, 4.0, 6.0])
+# print(x + y)
+# print(x - y)
+# print(x / y)
+# print(x / 2)
+
+# A = np.array([[1, 2], [3, 4]])
+# B = np.array([[3, 0], [0, 6]])
+# print(A + B)
+# print(A * B)
+
+# A = np.array([[1, 2], [3, 4]])
+# B = np.array([10, 20])
+# print(A * B)
+
+X = np.array([[51, 55], [14, 19], [0, 4]])
+print(X)
+print(X[0])
+print(X[0][1])
+
+for row in X:
+    print(row)
+
+X = X.flatten()
+print(X)
+print(X[np.array([0, 2, 4])])
+print(X > 15)
+print(X[X > 15])

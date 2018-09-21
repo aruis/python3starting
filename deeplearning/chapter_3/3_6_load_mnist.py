7e086896bfd5d4492a894bdba32dc9aa539dd544
Liu Rui <lovearuis@gmail.com>
2018-09-21 15:55:08 +0800

    add load_mnist

---
+++ deeplearning/chapter_3/3_6_load_mnist.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+import os
+import sys
+
+sys.path.append(os.pardir)  # 为了导入父目录的文件而进行的设定
+from dataset.mnist import load_mnist
+
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(t_test.shape)

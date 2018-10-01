2359d234f9876fa394f2c9951c552994d0dfd0bd
Liu Rui <lovearuis@gmail.com>
2018-10-01 09:48:34 +0800

    求偏导数

--- deeplearning/chapter_4/4_3_2.py
+++ deeplearning/chapter_4/4_3_2.py
@@ -11,6 +11,20 @@ def function_1(x):
     return 0.01 * x ** 2 + 0.1 * x
 
 
+def function_2(x):
+    return np.sum(x ** 2)
+
+
+# 求x0=3 ,x1 = 4时，关于x0的偏导数
+def tunction_tmp1(x0):
+    return x0 * x0 + 4 ** 2
+
+
+# 求x0=3 ,x1 = 4时，关于x1的偏导数
+def tunction_tmp2(x1):
+    return 3 ** 2 + x1 * x1
+
+
 x = np.arange(0.0, 20.0, 0.1)
 y = function_1(x)
 plt.xlabel("x")
@@ -20,3 +34,5 @@ plt.show()
 
 print(numerical_diff(function_1, 5))
 print(numerical_diff(function_1, 10))
+print(numerical_diff(tunction_tmp1, 3))
+print(numerical_diff(tunction_tmp2, 4))

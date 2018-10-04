39b5d82844644572432c637c817e247d93c8e7e3
Liu Rui <lovearuis@gmail.com>
2018-10-04 10:44:50 +0800

    update gradient_simplenet

--- deeplearning/chapter_4/4_4_2_gradient_simplenet.py
+++ deeplearning/chapter_4/4_4_2_gradient_simplenet.py
@@ -15,6 +15,27 @@ def softmax(a):
     return y
 
 
+def numerical_gradient(f, x):
+    h = 1e-4  # 0.0001
+    grad = np.zeros_like(x)
+
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x)  # f(x+h)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)  # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+
+        x[idx] = tmp_val  # 还原值
+        it.iternext()
+
+    return grad
+
+
 class simpleNet:
     def __init__(self):
         self.W = np.random.randn(2, 3)
@@ -32,3 +53,13 @@ class simpleNet:
 
 net = simpleNet()
 print(net.W)
+
+x = np.array([0.6, 0.9])
+p = net.predict(x)
+t = np.array([0, 0, 1])
+
+f = lambda w: net.loss(x, t)
+
+dW = numerical_gradient(f, net.W)
+
+print(dW)

e4ec775a82a3bbf7c72ace9d7fe794fc48d516af
Liu Rui <lovearuis@gmail.com>
2018-10-06 14:10:43 +0800

    update 4_5_train_neuralnet.py

--- deeplearning/chapter_4/4_5_train_neuralnet.py
+++ deeplearning/chapter_4/4_5_train_neuralnet.py
@@ -7,14 +7,19 @@ from dataset.mnist import load_mnist
 from two_layer_net import TwoLayerNet
 
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
-train_loss_list = []
 
-iters_num = 1000
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+iters_num = 1000  # 适当设定循环的次数
 train_size = x_train.shape[0]
 batch_size = 100
 learning_rate = 0.1
 
-network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+iter_per_epoch = max(train_size / batch_size, 1)
 
 for i in range(iters_num):
     batch_mask = np.random.choice(train_size, batch_size)
@@ -28,3 +33,11 @@ for i in range(iters_num):
 
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
+
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+

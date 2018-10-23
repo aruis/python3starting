8b9e1d84b3d6acac4303e890f0d27865b2793535
Liu Rui <lovearuis@gmail.com>
2018-10-23 09:02:25 +0800

    learning mnist

--- deep_learning_with_python/chapter_2/2_1.py
+++ deep_learning_with_python/chapter_2/2_1.py
@@ -1,8 +1,9 @@
+from keras import models
+from keras import layers
+from keras.utils import to_categorical
 from keras.datasets import mnist
 
-(train_images, train_lables), (test_images, test_labels) = mnist.load_data()
-
-print(train_images.shape)
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
 train_images = train_images.reshape((60000, 28 * 28))
 train_images = train_images.astype('float32') / 255
@@ -10,5 +11,16 @@ train_images = train_images.astype('float32') / 255
 test_images = test_images.reshape((10000, 28 * 28))
 test_images = test_images.astype('float32') / 255
 
-print(train_images.shape)
-print(test_images.shape)
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+
+network = models.Sequential()
+network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,)))
+network.add(layers.Dense(10, activation='softmax'))
+
+network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+network.fit(train_images, train_labels, epochs=5, batch_size=128)
+
+test_loss, test_acc = network.evaluate(test_images, test_labels)
+print('test_acc:', test_acc)
